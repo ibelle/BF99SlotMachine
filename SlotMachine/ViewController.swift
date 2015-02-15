50ac033380613151c2a8a8e3c84eee183f31e1ba
HUGE | Isaiah Belle <ibelle@ibelle-ny-05589.home>
2015-02-15 01:37:32 -0500

    Initial container setup
    Initial container setup

--- SlotMachine/ViewController.swift
+++ SlotMachine/ViewController.swift
@@ -14,14 +14,42 @@ class ViewController: UIViewController {
     var thirdContainer: UIView!
     var fourthContainer: UIView!
     
+    let kMarginForView:CGFloat=10.0 //Margin for left and right of view
+    let kSixth:CGFloat = 1.0/6.0 //scalar for view height e.g. 1/6th hieght of container view
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setUpContainerViews()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setUpContainerViews(){
+        //First SubView
+       self.firstContainer = UIView(frame: CGRect(x: self.view.bounds.origin.x + kMarginForView, y: self.view.bounds.origin.y, width: self.view.bounds.width - (kMarginForView * 2), height: self.view.bounds.height * kSixth))
+        self.firstContainer.backgroundColor = UIColor.redColor()
+        self.view.addSubview(self.firstContainer)
+        
+        //Second SubView
+        self.secondContainer = UIView(frame: CGRect(x: self.view.bounds.origin.x + kMarginForView, y: firstContainer.frame.height, width: self.view.bounds.width - (kMarginForView * 2), height: self.view.bounds.height * (3 * kSixth)))
+        self.secondContainer.backgroundColor = UIColor.blackColor()
+        self.view.addSubview(self.secondContainer)
+        
+        
+        //Third SubView
+        self.thirdContainer = UIView(frame: CGRect(x: self.view.bounds.origin.x + kMarginForView, y: secondContainer.frame.maxY, width: self.view.bounds.width - (kMarginForView * 2), height: self.view.bounds.height * kSixth))
+        self.thirdContainer.backgroundColor = UIColor.lightGrayColor()
+        self.view.addSubview(self.thirdContainer)
+        
+        //Forth SubView
+        self.fourthContainer = UIView(frame: CGRect(x: self.view.bounds.origin.x + kMarginForView, y: thirdContainer.frame.maxY, width: self.view.bounds.width - (kMarginForView * 2), height: self.view.bounds.height * kSixth))
+        self.fourthContainer.backgroundColor = UIColor.blueColor()
+        self.view.addSubview(self.fourthContainer)
     }
 
 
